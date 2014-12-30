829a92c21a64fa3018978e3a3f44c26f857166df
milesoldenburg <milesoldenburg@gmail.com>
2014-12-30 16:23:34 -0500

    Removed .gif from selection as they don't have the 1280 res

--- data/content-script.js
+++ data/content-script.js
@@ -1,11 +1,11 @@
 // When an image is right clicked
 self.on('context', function(node){
     // Only show the context menu if the img is from tumblr and already not at max resolution
-    return (node.src.search(/^(https?:\/\/)[0-9]+(\.media\.tumblr\.com\/)(\w)*(\/)?(tumblr_)(\w)*_(?!1280)(\d)*\.(jpg|gif|png|jpeg)$/g) > -1) ? true : false;
+    return (node.src.search(/^(https?:\/\/)[0-9]+(\.media\.tumblr\.com\/)(\w)*(\/)?(tumblr_)(\w)*_(?!1280)(\d)*\.(jpg|png|jpeg)$/g) > -1) ? true : false;
 });
 
 // When the context menu item is selected
 self.on('click', function(node){
     // Send url to add on with max res image replaced
-    self.postMessage(node.src.replace(/(\d)+(?=\.(jpg|gif|png|jpeg)$)/g, '1280'));
+    self.postMessage(node.src.replace(/(\d)+(?=\.(jpg|png|jpeg)$)/g, '1280'));
 });
